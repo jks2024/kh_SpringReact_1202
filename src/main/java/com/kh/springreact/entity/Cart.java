16edcd74fa04c005629ec19905aced10926af0b7
jks2024 <jks2024@gmail.com>
2022-12-06 10:39:32 +0900

    cart add

---
+++ src/main/java/com/kh/springreact/entity/Cart.java
@@ -0,0 +1,23 @@
+package com.kh.springreact.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "cart")
+@Getter
+@Setter
+@ToString
+public class Cart {
+    @Id
+    @Column(name = "cart_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @OneToOne // 일대일 매핑이며, 장바구니 엔티티가 회원 엔티티를 참조하는 일애일 단방향 매핑
+    @JoinColumn(name = "membet_id")
+    private Member member;
+}
